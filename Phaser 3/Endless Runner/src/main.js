fb7f3bea1e852e844cc863a34c50ab8a05248277
Zehao Dong <zdong18@ucsc.edu>
2023-05-18 17:56:28 -0700

    Endless Runner

--- Phaser 3/Endless Runner/src/main.js
+++ Phaser 3/Endless Runner/src/main.js
@@ -1,12 +1,4 @@
-// Nathan Altice
-// Phaser 3 gamepad example inspired by Taito's Speed Race (1974)
-// Portions of code adapted from official Phaser 3 gamepad examples w/ new comments added
-// 3/28/23
-// car sprites adapted from tokka's Top Down Cars Sprite Pack 1.0: https://tokka.itch.io/top-down-car
-// road bg adapted from Alucard's 2D Top Down Highway Background: https://opengameart.org/content/2d-top-down-highway-background
-// car horn sfx from hasunier10 https://pixabay.com/sound-effects/car-horn-beepsmp3-14659/
-
-// prevent speeding 🚘
+// Zehao Dong
 "use strict";
 
 var config = {
