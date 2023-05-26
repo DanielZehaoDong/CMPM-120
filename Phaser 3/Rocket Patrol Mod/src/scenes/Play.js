daa6f6e35aaa5a51f44b26f1bd79061c5af19bbc
Zehao Dong <zdong18@ucsc.edu>
2023-05-25 17:55:30 -0700

    revise restart

--- Phaser 3/Rocket Patrol Mod/src/scenes/Play.js
+++ Phaser 3/Rocket Patrol Mod/src/scenes/Play.js
@@ -68,9 +68,9 @@ class Play extends Phaser.Scene {
         // check key input for restart
         if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
             this.scene.restart();
+        }
         if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
             this.scene.start("menuScene");
-        }
         }
         this.starfield.tilePositionX -= 4;
         if (!this.gameOver) {               
