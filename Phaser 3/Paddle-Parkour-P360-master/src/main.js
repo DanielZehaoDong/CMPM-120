fb5ca0b69b2b5ac3acb204cc372d642f4c3783bb
Zehao Dong <zdong18@ucsc.edu>
2023-06-03 20:23:30 -0700

    Build basic set up

--- Phaser 3/Paddle-Parkour-P360-master/src/main.js
+++ Phaser 3/Paddle-Parkour-P360-master/src/main.js
@@ -1,52 +1,25 @@
-// Nathan Altice
-// Paddle Parkour P360
-// An endless dodging game
-// Barrier prefab adapted from Travis Faas, An Introduction to HTML5 Game Development with Phaser.js (2017)
-// Original: 4/20/17 (Phaser CE version)
-// Updated: 5/6/23 (Phaser 3.60)
+// Zehao Dong
+// Endless Runner
+// 20 Hours
+// I create this game for a endless running game. The character should avoid different obstructions to survive.
+// But you dont have ability to fight back. So keep suvive and good luck!
 
-// keep me honest
-'use strict';
 
 // define and configure main Phaser game object
 let config = {
-    parent: 'myGame',
     type: Phaser.AUTO,
-    height: 640,
-    width: 960,
-    scale: {
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
+    width: 800,
+    height: 500,
+    scene: [ Menu, Play, Tutorial],
     physics: {
-        default: 'arcade',
-        arcade: {
-            //debug: true,
-            gravity: {
-                x: 0,
-                y: 0
-            }
+        default:"arcade",
+        arcade:{
+            //debug: True
         }
-    },
-    scene: [ Load, Title, Play, GameOver ]
+    }
 }
-
-// uncomment the following line if you need to purge local storage data
-//localStorage.clear();
-
-// define game
 let game = new Phaser.Game(config);
-
-// define globals
 let centerX = game.config.width/2;
 let centerY = game.config.height/2;
-let w = game.config.width;
-let h = game.config.height;
-const textSpacer = 64;
-let paddle = null;
-const paddleWidth = 16;
-const paddleHeight = 128;
-const paddleVelocity = 150;
-let level;
-let highScore;
-let newHighScore = false;
-let cursors;
+// reserve keyboard vars
+let keySPACE, keyLeft, keyRight, keyT;
