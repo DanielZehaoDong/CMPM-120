24cbd7a726b5f3622b554ca8d75a5f11314ff191
Zehao Dong <zdong18@ucsc.edu>
2023-06-09 16:41:08 -0700

    delete space button input

--- Phaser 3/Endless Runner/src/scenes/Tutorial.js
+++ Phaser 3/Endless Runner/src/scenes/Tutorial.js
@@ -21,24 +21,12 @@ class Tutorial extends Phaser.Scene {
         this.add.text(game.config.width/2+100,game.config.height/5+60+100+100, 'Keep alive!', menuConfig).setOrigin(0.5,0.5);
         this.add.text(game.config.width/2+100,game.config.height/5+60+100+100+50, 'Good luck bro!', menuConfig).setOrigin(0.5,0.5);
         menuConfig.fontSize=30;
-        this.add.text(game.config.width/2,game.config.height*9/10, "Press 'Space' to start and 'Enter' back to menu!", menuConfig).setOrigin(0.5,0.5);
+        this.add.text(game.config.width/2,game.config.height*9/10, "Press Enter to back to menu!", menuConfig).setOrigin(0.5,0.5);
         keyENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
-        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
     update() {
         if (Phaser.Input.Keyboard.JustDown(keyENTER)) {
             this.scene.start('menuScene');    
-        }
-        if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
-            let textureManager = this.textures;
-            this.game.renderer.snapshot((snapshotImage) => {
-                if(textureManager.exists('titlesnapshot')) {
-                    textureManager.remove('titlesnapshot');
-                }
-                textureManager.addImage('titlesnapshot', snapshotImage);
-            });
-            this.bgmMenu.stop();
-            this.scene.start('playScene'); 
         }
     }
 }
