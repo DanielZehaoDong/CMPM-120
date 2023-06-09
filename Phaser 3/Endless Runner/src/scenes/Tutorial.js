1e6ee4f5e7563d24d5940deec60a250693b5826c
Zehao Dong <zdong18@ucsc.edu>
2023-06-09 01:40:09 -0700

    change tutorial

--- Phaser 3/Endless Runner/src/scenes/Tutorial.js
+++ Phaser 3/Endless Runner/src/scenes/Tutorial.js
@@ -13,7 +13,7 @@ class Tutorial extends Phaser.Scene {
             fontFamily: 'cursive',
             fontSize: '24px',
             color: '#ffa500',
-            align: 'right',
+            align: 'left',
         fixedWidth: 0
         }
         this.add.text(game.config.width/2+100,game.config.height/5+60, 'Try to avoid the obstructions', menuConfig).setOrigin(0.5,0.5);
@@ -21,17 +21,17 @@ class Tutorial extends Phaser.Scene {
         this.add.text(game.config.width/2+100,game.config.height/5+60+100+100, 'Keep alive!', menuConfig).setOrigin(0.5,0.5);
         this.add.text(game.config.width/2+100,game.config.height/5+60+100+100+50, 'Good luck bro!', menuConfig).setOrigin(0.5,0.5);
         menuConfig.fontSize=30;
-        this.add.text(game.config.width/2,game.config.height*9/10, "Press 'Space' to start and '<-' back to menu!", menuConfig).setOrigin(0.5,0.5);
+        this.add.text(game.config.width/2,game.config.height*9/10, "Press 'Space' to start and 'Enter' back to menu!", menuConfig).setOrigin(0.5,0.5);
+        keyENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
     }
     update() {
+        if (Phaser.Input.Keyboard.JustDown(keyENTER)) {
+            this.scene.start('menuScene');    
+        }
         if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
             this.bgmMenu.stop();
-            this.scene.start('playScene'); 
-        }
-        if (Phaser.Input.Keyboard.JustDown(keyLeft)) {
-            this.scene.start('menuScene'); 
+            this.scene.start('playScene');    
         }
     }
 }
