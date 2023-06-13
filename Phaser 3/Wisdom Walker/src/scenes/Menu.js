2fa4ca69540745536d465f61fbdf3f1aba683bc0
Zehao Dong <zdong18@ucsc.edu>
2023-06-12 19:26:43 -0700

    first try

--- Phaser 3/Wisdom Walker/src/scenes/Menu.js
+++ Phaser 3/Wisdom Walker/src/scenes/Menu.js
@@ -1,4 +1,4 @@
-class Menu extends Phaser.Scene {
+/*class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
@@ -48,3 +48,4 @@ class Menu extends Phaser.Scene {
         }
     }
 }
+*/
