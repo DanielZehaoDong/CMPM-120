3551bdd737a4923a44fa48b003eefb8f8f960be9
Zehao Dong <zdong18@ucsc.edu>
2023-06-13 17:41:18 -0700

    add one more tilemap

--- Phaser 3/Wisdom Walker/src/scenes/Color.js
+++ Phaser 3/Wisdom Walker/src/scenes/Color.js
@@ -3,4 +3,93 @@ class Color extends Phaser.Scene {
         super({key: 'colorScene'})
         this.VEL = 100;
     }
+    preload(){
+        this.load.path = './assets/';
+        this.load.spritesheet('bob', 'bob.png', {
+            frameWidth: 32,
+            frameHeight: 48
+        });
+        this.load.image('tilesetImage', 'gameTileset.png');
+        this.load.tilemapTiledJSON('gameMap2JSON', 'gameMap2.json');
+        this.load.spritesheet('portal','portal.png', {
+            frameWidth: 48,
+            frameHeight: 48,
+        });
+        this.load.audio('bgmPlay', 'bgmPlay.wav')
+    }
+
+    create(){
+        const map = this.add.tilemap('gameMap2JSON');
+        const tileset = map.addTilesetImage('tileset', 'tilesetImage');
+
+        // add layer
+        const bgLayer = map.createLayer('Background', tileset, 0,0);
+        const terrainLayer = map.createLayer('Terrain', tileset, 0,0);
+        const treeLayer = map.createLayer('Tree', tileset, 0,0).setDepth(10);
+
+         // add bob
+         this.bob = this.physics.add.sprite(32, 32, 'bob', 0);
+         this.anims.create({
+             key: 'walk',
+             frameRate: 8,
+             repeat: -1,
+             frames: this.anims.generateFrameNumbers('bob', {
+                 start: 0,
+                 end: 1
+             })
+         });
+         this.bob.play('walk');
+         this.bob.body.setCollideWorldBounds(true);
+
+         // add portal
+         this.portal = this.physics.add.sprite(48, 48, 'portal', 0);
+         this.anims.create({
+             key: 'rotate',
+             frameRate: 4,
+             repeat: -1,
+             frames: this.anims.generateFrameNumbers('portal', {
+                 start: 0,
+                 end: 1
+             })
+         });
+         this.portal.play('rotate');
+
+         //add background music
+         this.bgmPlay = this.sound.add("bgmPlay", { loop: true });
+         this.bgmPlay.play();
+
+         
+
+         // enable collision
+        terrainLayer.setCollisionByProperty({collides: true});
+        treeLayer.setCollisionByProperty({collides: true});
+        
+        this.physics.add.collider(this.bob, terrainLayer);
+        this.physics.add.collider(this.bob, treeLayer);
+
+         // cameras
+         this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+         this.cameras.main.startFollow(this.slime, true, 0.25, 0.25);
+         this.physics.world.bounds.setTo(0, 0, map.widthInPixels, map.heightInPixels);
+
+         // input
+        this.cursors = this.input.keyboard.createCursorKeys();
+ 
+    }
+
+    update(){
+        this.direction = new Phaser.Math.Vector2(0);
+        if(this.cursors.left.isDown){
+            this.direction.x = -1;
+        }else if(this.cursors.right.isDown){
+            this.direction.x = 1;
+        }
+        if(this.cursors.up.isDown){
+            this.direction.y = -1;
+        }else if(this.cursors.down.isDown){
+            this.direction.y = 1;
+        }
+        this.direction.normalize();
+        this.bob.setVelocity(this.VEL * this.direction.x , this.VEL * this.direction.y);
+    }
 }
