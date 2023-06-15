65bdcc008902e6e10f3b8713e5dd1460e1643a38
Zehao Dong <zdong18@ucsc.edu>
2023-06-15 01:00:36 -0700

    debug

--- Phaser 3/Wisdom Walker/src/scenes/Color.js
+++ Phaser 3/Wisdom Walker/src/scenes/Color.js
@@ -5,16 +5,14 @@ class Color extends Phaser.Scene {
     }
     preload(){
         this.load.path = './assets/';
-        this.load.spritesheet('bob', 'bob.png', {
-            frameWidth: 32,
-            frameHeight: 48
-        });
+        this.load.image('bob', 'bob.png');
         this.load.image('tilesetImage', 'gameTileset.png');
         this.load.tilemapTiledJSON('gameMap2JSON', 'gameMap2.json');
         this.load.spritesheet('portal','portal.png', {
             frameWidth: 48,
             frameHeight: 48,
         });
+        this.load.image('book', 'book.png');
         this.load.audio('bgmPlay', 'bgmPlay.wav')
     }
 
@@ -22,43 +20,33 @@ class Color extends Phaser.Scene {
         const map = this.add.tilemap('gameMap2JSON');
         const tileset = map.addTilesetImage('gameTileset', 'tilesetImage');
 
+        //add background music
+        this.bgmPlay = this.sound.add("bgmPlay", { loop: true });
+        this.bgmPlay.play();
+
         // add layer
         const bgLayer = map.createLayer('Background', tileset, 0,0);
         const terrainLayer = map.createLayer('Terrain', tileset, 0,0);
         const treeLayer = map.createLayer('Tree', tileset, 0,0).setDepth(10);
 
-         // add bob
-         this.bob = this.physics.add.sprite(32, 32, 'bob', 0);
-         this.anims.create({
-             key: 'walk',
-             frameRate: 8,
-             repeat: -1,
-             frames: this.anims.generateFrameNumbers('bob', {
-                 start: 0,
-                 end: 1
-             })
-         });
-         this.bob.play('walk');
-         this.bob.body.setCollideWorldBounds(true);
 
          // add portal
-         this.portal = this.physics.add.sprite(48, 48, 'portal', 0);
+         this.portal = this.physics.add.sprite(80, 752, 'portal');
          this.anims.create({
-             key: 'rotate',
-             frameRate: 4,
-             repeat: -1,
-             frames: this.anims.generateFrameNumbers('portal', {
-                 start: 0,
-                 end: 1
-             })
-         });
+            key: "rotate",
+            frameRate: 8,
+            frames: this.anims.generateFrameNumbers("portal", { start: 0, end: 1 }),
+            repeat: -1
+        });
          this.portal.play('rotate');
 
-         //add background music
-         this.bgmPlay = this.sound.add("bgmPlay", { loop: true });
-         this.bgmPlay.play();
+          // add bob
+          this.bob = this.physics.add.sprite(32, 272, 'bob');
+          this.bob.body.setCollideWorldBounds(true);
 
-         
+         // add book
+         this.book = this.physics.add.sprite(752, 48, 'book', 0);
+         this.book.destroyed = false;
 
          // enable collision
         terrainLayer.setCollisionByProperty({collides: true});
@@ -69,7 +57,7 @@ class Color extends Phaser.Scene {
 
          // cameras
          this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-         this.cameras.main.startFollow(this.slime, true, 0.25, 0.25);
+         this.cameras.main.startFollow(this.bob, true, 0.25, 0.25);
          this.physics.world.bounds.setTo(0, 0, map.widthInPixels, map.heightInPixels);
 
          // input
@@ -91,5 +79,15 @@ class Color extends Phaser.Scene {
         }
         this.direction.normalize();
         this.bob.setVelocity(this.VEL * this.direction.x , this.VEL * this.direction.y);
+
+        this.physics.world.collide(this.bob, this.book, this.bookCollect, null, this);
+    }
+
+    bookCollect(){
+        this.book.destroyed = true;
+        this.physics.world.collide(this.bob, this.portal, this.portalTrigger, null, this);
+    }
+    portalTrigger(){
+        this.scene.start('gameoverScene')
     }
 }
--- Phaser 3/Wisdom Walker/src/scenes/Overworld.js
+++ Phaser 3/Wisdom Walker/src/scenes/Overworld.js
@@ -57,7 +57,7 @@ class Overworld extends Phaser.Scene {
 
          // cameras
          this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-         this.cameras.main.startFollow(this.slime, true, 0.25, 0.25);
+         this.cameras.main.startFollow(this.bob, true, 0.25, 0.25);
          this.physics.world.bounds.setTo(0, 0, map.widthInPixels, map.heightInPixels);
 
          // input
